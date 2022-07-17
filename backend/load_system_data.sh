f3369be009aa1b98089dbaf828e71f520a05391c
aleksandrtikhonov <aleksandrtikhonov91@gmail.com>
2022-07-17 20:45:34 +0300

    fix

--- backend/load_system_data.sh
+++ backend/load_system_data.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-python manage.py importcsv data/ingredients.csv Ingredient --no-inputпше
+python manage.py importcsv data/ingredients.csv Ingredient --no-input
