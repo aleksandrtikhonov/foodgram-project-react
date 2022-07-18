6092ee42d49678b7c36b36ea5c73fb57ebd9f5cf
aleksandrtikhonov <aleksandrtikhonov91@gmail.com>
2022-07-18 12:27:12 +0300

    chek filter

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -18,11 +18,9 @@ class RecipeFilter(FilterSet):
     is_in_shopping_cart = NumberFilter(
         field_name='is_in_shopping_cart', method='get_is_in_shopping_cart'
     )
-    recipes_limit = NumberFilter(
-        field_name='recipes_limit', method='get_recipes_limit'
-    )
 
     def get_is_favorited(self, queryset, name, value):
+        print(value, 'sssssss', queryset)
         if value == 1 and not self.request.user.is_anonymous:
             return queryset.filter(favorites_recipe__user=self.request.user)
         return queryset
@@ -32,9 +30,6 @@ class RecipeFilter(FilterSet):
             return queryset.filter(carts_recipe__user=self.request.user)
         return queryset
 
-    def get_recipes_limit(self, queryset, name, value):
-        print(queryset)
-        return queryset
 
     class Meta:
         model = Recipe
@@ -43,5 +38,4 @@ class RecipeFilter(FilterSet):
             'tags',
             'is_favorited',
             'is_in_shopping_cart',
-            'recipes_limit'
         )
