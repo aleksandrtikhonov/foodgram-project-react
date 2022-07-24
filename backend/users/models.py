c50b8fc94cf78e7932b8391626944171246d6ac6
aleksandrtikhonov <aleksandrtikhonov91@gmail.com>
2022-07-24 22:56:45 +0300

    pep8

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import CheckConstraint, F, UniqueConstraint, Q
 
-
 class User(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
 
